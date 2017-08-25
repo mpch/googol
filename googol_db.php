d58f3fa8ebd0b272ed79a992d028621f0e15cdda
Bronco <broncowdd@users.noreply.github.com>
2017-08-25 18:32:23 +0200

    v2.0 adding googols db files

---
+++ googol_db.php
@@ -0,0 +1,7 @@
+<?php
+// This file contains a list of googol instances'url. Those googols will be randomly called in case of banishment from google
+// You can edit or add (don't add final slash)
+$googol_db=[
+	'https://googol.warriordudimanche.net',
+	'https://search.green-effect.fr',
+];
---
+++ googol_db_files_url.php
@@ -0,0 +1,8 @@
+<?php
+// This file contains the url of the distant db files
+// you can change or add.
+// These urls are used to get the googol_db.php file if there's no local version.
+$googol_db_files_url=[
+	'http://googol.warriordudimanche.net/official_googols_db.ini',
+	'https://raw.githubusercontent.com/broncowdd/googol/master/official_googols_db.ini',
+];
